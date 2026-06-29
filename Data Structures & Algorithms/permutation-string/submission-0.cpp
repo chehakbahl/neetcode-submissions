37bfd4906ab15bf8613c9092a33da23805971b23
Chehak Bahl <chehak.bahl@gmail.com>
2026-06-29 15:55:00 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size()>s2.size()) return false;
+
+        vector<int> freq1(26,0),freq2(26,0);
+        for(char c:s1){
+            freq1[c-'a']++;
+        }
+        int left=0;
+        for(int right=0;right<s2.size();right++){
+            freq2[s2[right]-'a']++;
+
+            if(right-left+1>s1.size()){//shrink window
+                freq2[s2[left]-'a']--;
+                left++;
+            }
+            if(freq1==freq2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
